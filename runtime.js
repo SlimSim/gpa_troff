0630efacd9f649f9a3085a9f81813af81723179c
SlimSim Stationara Jobb <slimsimapps@gmail.com>
2015-04-14 08:22:31 +0200

    added GNU gpl to runtime

--- runtime.js
+++ runtime.js
@@ -1,5 +1,19 @@
-// Use the runtime event listeners to set a window property indicating whether the
-// app was launched normally or as a result of being restarted
+/*
+This file is part of Troff.
+
+Troff is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Troff is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Troff.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 chrome.app.runtime.onLaunched.addListener(function(data) {
   chrome.app.window.create(
